8446344d0305e60edad0cb511d2bedf634776ed0
Gabriel Dennett <50601660+GFD1998@users.noreply.github.com>
2023-05-31 22:08:18 -0400

    Merge Complete

--- config/routes.php
+++ config/routes.php
@@ -21,6 +21,7 @@ return function (App $app) {
         return $response;
     });
 
+    
     $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
         $name = $args['name'];
         $response->getBody()->write("Hello, $name");
