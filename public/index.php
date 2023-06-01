0d5ca3a63cc04ce1731c7c3ccaa0c6e8cff33f61
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-05-31 21:31:17 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 //require bootstrap
 
 (require __DIR__ . '/../config/bootstrap.php')->run();
