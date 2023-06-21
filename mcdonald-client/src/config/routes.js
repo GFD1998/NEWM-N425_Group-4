dac8e5e0bc4cfdd0e59da9c529ee51d4dec7a516
MychalWood <116838101+MychalWood@users.noreply.github.com>
2023-06-20 22:03:20 -0400

    Update routes.js

--- mcdonald-client/src/config/routes.js
+++ mcdonald-client/src/config/routes.js
@@ -1,12 +1,13 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/home";
 import NoMatch from "../pages/nomatch";
 import MenuItem from "../pages/menuitem/menuitem";
+import MenuItems from "../pages/menuitem/menuitems";
 import Allergen from "../pages/allergen/allergen";
 import Ingredient from "../pages/ingredient/ingredient";
+import Ingredients from "../pages/ingredient/ingredients";
 import NutritionalInformation from "../pages/nutritionalInformation/nutritionalinformation";
-// import Classes from "../pages/class/classes";
 import {AuthProvider} from "../services/useAuth";
 import RequireAuth from "../components/RequireAuth";
 import Signin from "../pages/auth/signin";
@@ -15,39 +16,44 @@ import Signup from "../pages/auth/signup";
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <AuthProvider>
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home/>}/>
-                        <Route path="/menuitem" element={
+                        <Route path="menuitems" element={
                             <RequireAuth>
-                                <MenuItem />
+                                <MenuItems/>
+                            </RequireAuth>
+                        }>
+                            <Route index element={<p>Select a professor to view details.</p>}/>
+                            <Route path=":menuitemID" element={<MenuItem/>}>
+                                <Route path="ingredients" element={<Ingredients />}/>
+                            </Route>
+                        </Route>
+                        <Route path="allergen" element={
+                            <RequireAuth>
+                                <Allergen />
                             </RequireAuth>
                         }/>
-                        <Route path="/allergen" element={
+                        <Route path="ingredients" element={
                             <RequireAuth>
-                                <Allergen/>
+                                <Ingredients />
                             </RequireAuth>
                         }/>
-                        <Route path="/ingredient" element={
+                        <Route path="nutritionalinformation" element={
                             <RequireAuth>
-                                <Ingredient/>
-                            </RequireAuth>
-                        }/>
-                        <Route path="/nutritionalinformation" element={
-                            <RequireAuth>
-                                <NutritionalInformation/>
+                                <NutritionalInformation />
                             </RequireAuth>
                         }/>
                         <Route path="/signin" element={<Signin/>}/>
                         <Route path="/signout" element={<Signout/>}/>
                         <Route path="/signup" element={<Signup/>}/>
-                        {/* <Route path="*" element={<NoMatch/>}/> */}
+                        <Route path="*" element={<NoMatch/>}/>
                     </Route>
                 </Routes>
             </AuthProvider>
-        </BrowserRouter>
+        </Router>
     );
 };
 
